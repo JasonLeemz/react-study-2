3188bba121e18938f84e63d8b2b5bfcda0260754
limingze <limingze610@163.com>
2016-10-13 17:20:33 +0800

    jsx style

--- app/index.js
+++ app/index.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-class App extends React.Component{
-	constructor(){
-		super();
-		this.state = {
-
-		};
-	}
-
-	render(){
-		return (
-				<div>
-					'Hello Jason!!'
-				</div>
-			);
-	}
+let style = {
+    fontSize: '25px',
+    color: '#ff0000'
+};
+let MyComponent = () => (
+    <div >Hello, World!</div>
+);
+let MyComponent2 = () => {
+    return (
+        <div >Hello, World!2</div>
+    );
+};
+class MyComponent3 extends React.Component {
+    render() {
+        let thisStyle = this.props.style;
+        return (
+            <div style={thisStyle}>Hello, World!3</div>
+        );
+    }
 }
 
-ReactDom.render(<App />,document.getElementById('app'));
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Jason!!</h1>
+                <MyComponent style={style}/>
+                <MyComponent2 style={style}/>
+                <MyComponent3 style={style}/>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
